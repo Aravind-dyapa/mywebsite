ae9646f8ed02154dec46b9c37504f4422e3ef6b9
Aravind-dyapa <aravindreddydyapa@gmail.com>
2024-11-09 21:24:26 +0530

    1st change

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-u*ob+fmk14v^k+!2n6orwh_p$$=%pc$)$8ea831kb)waa^c5lz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mywebsite.onrender.com','127.0.0.1']
+
 
 
 # Application definition
